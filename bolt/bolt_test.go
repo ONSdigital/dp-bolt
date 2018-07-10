9e5c26eb526904a351831c2172eb526cf48d286e
Dave Llewellyn <439837@gmail.com>
2018-07-10 17:20:04 +0100

    added QueryForResult_QueryNeoError unit test

--- bolt/bolt_test.go
+++ bolt/bolt_test.go
@@ -40,12 +40,9 @@ func TestDB_QueryForResultOpenConnErr(t *testing.T) {
 
 		Convey("when QueryForResult is called", func() {
 			var extractCalls []*Result
-			closure := func(r *Result) error {
-				extractCalls = append(extractCalls, r)
-				return nil
-			}
+			mockExtractor := mockResultExtractor(extractCalls, nil)
 
-			err := db.QueryForResult("", nil, closure)
+			err := db.QueryForResult("", nil, mockExtractor)
 
 			Convey("then an error is returned", func() {
 				So(err, ShouldNotBeNil)
@@ -55,4 +52,46 @@ func TestDB_QueryForResultOpenConnErr(t *testing.T) {
 			})
 		})
 	})
+}
+
+func TestDB_QueryForResult_QueryNeoError(t *testing.T) {
+	Convey("given conn.QueryNeo return an error", t, func() {
+		conn := &mock.NeoConnMock{
+			QueryNeoFunc: func(query string, params map[string]interface{}) (golangNeo4jBoltDriver.Rows, error) {
+				return nil, errTest
+			},
+			CloseFunc: closeNoErr,
+		}
+		pool := &mock.DBPoolMock{
+			OpenPoolFunc: func() (golangNeo4jBoltDriver.Conn, error) {
+				return conn, nil
+			},
+		}
+		db := New(pool)
+
+		Convey("when QueryForResult is called", func() {
+			var extractCalls []*Result
+			mockExtractor := mockResultExtractor(extractCalls, nil)
+
+			err := db.QueryForResult("", nil, mockExtractor)
+
+			Convey("then an error is returned and the connection is closed", func() {
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldResemble, errors.WithMessage(errTest, "error executing neo4j query").Error())
+				So(pool.OpenPoolCalls(), ShouldHaveLength, 1)
+				So(conn.QueryNeoCalls(), ShouldHaveLength, 1)
+				So(conn.QueryNeoCalls()[0].Query, ShouldEqual, "")
+				So(conn.QueryNeoCalls()[0].Params, ShouldEqual, nil)
+				So(conn.CloseCalls(), ShouldHaveLength, 1)
+				So(extractCalls, ShouldHaveLength, 0)
+			})
+		})
+	})
+}
+
+func mockResultExtractor(extractCalls []*Result, err error) ResultExtractorClosure {
+	return func(r *Result) error {
+		extractCalls = append(extractCalls, r)
+		return err
+	}
 }
